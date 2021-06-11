4daa5868406a224f0c960bbaf8b62de64a8cc971
x33dan <85710194+x33dan@users.noreply.github.com>
2021-06-11 10:49:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ b2.onclick = function() {
   randomSpeed();
   mainFunction();
   vP = 3;
-  sc1 = 30;
+  sc1 = 1000;
 }
 
 b3.onclick = function() {
@@ -133,7 +133,7 @@ b3.onclick = function() {
   randomSpeed();
   mainFunction();
   vP = 3;
-  sc1 = 70;
+  sc1 = 1000;
 }
 
 
